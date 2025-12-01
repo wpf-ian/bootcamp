1a646f817a52edf0b824e341cec50ad4d4d35fe7
Ian <ianpf.wong@gmail.com>
2025-12-01 17:59:15 +0800

    Welcome to Bootcamp

---
+++ demo-helloworld/src/Helloworld.java
@@ -0,0 +1,10 @@
+public class Helloworld{
+  public static void main(String[] args){
+    //Jot Notes
+    //Tools
+    // 1.System.out.println("")
+    //Every lines of Java code must end with semicolon ;
+    System.out.println("Hello World");
+    System.out.println("Hi");
+  }
+}
